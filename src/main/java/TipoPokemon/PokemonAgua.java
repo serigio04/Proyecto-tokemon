ce98b6c9c9b6e1deeac1ade88f5c8628a019e8fb
serigio04 <serigi.gomar15@gmail.com>
2024-09-30 22:34:26 -0600

    [ADD] Clase Pokemon tipo agua

---
+++ src/main/java/TipoPokemon/PokemonAgua.java
@@ -0,0 +1,13 @@
+package main.java.TipoPokemon;
+
+public class PokemonAgua extends Pokemon{
+    
+    public PokemonAgua (int numPokedex, String nombre, double vida, double experiencia, int nivel){
+        super(numPokedex, nombre, vida, experiencia, nivel);
+    }
+
+    public void ataqueFuego(double vida){
+        this.vida -= 2;
+        System.out.println( this.nombre + " ha recibido un ataque");
+    }
+}
